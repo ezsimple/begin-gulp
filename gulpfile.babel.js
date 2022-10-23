86cce8e97b197794b97817d7c6f491473fd890eb
mhlee <mkeasy@gmail.com>
2022-10-23 21:34:08 +0900

    테스트중

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,24 +1,24 @@
-import gulp from 'gulp';
-import gpug from 'gulp-pug';
-import { deleteAsync } from 'del';
-import gws from 'gulp-webserver';
+import gulp from "gulp";
+import gpug from "gulp-pug";
+import { deleteAsync } from "del";
+import gws from "gulp-webserver";
 
 const routes = {
   pug: {
-    src: 'src/*.pug',
-    dest: 'build',
-    watch: 'src/**/*.pug',
+    src: "src/views/pages/*.pug",
+    dest: "build",
+    watch: "src/**/*.pug",
   },
 };
 
 // Funtions
-const clean = () => deleteAsync(['build/']);
+const clean = () => deleteAsync(["build/"]);
 
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
 const webServer = () => {
-  gulp.src('build').pipe(gws({ livereload: true, open: true }));
+  gulp.src("build").pipe(gws({ livereload: true, open: true }));
 };
 
 const watch = () => gulp.watch(routes.pug.watch, pug);
@@ -29,4 +29,4 @@ const assets = gulp.series([pug]);
 const render = gulp.parallel([webServer, watch]);
 
 const dev = gulp.series([prepare, assets, render]);
-gulp.task('default', dev);
+gulp.task("default", dev);
